d12e99ad406a6a12f98d491f21c184f8e0267bda
Shivansh Saini <shivanshs9@gmail.com>
2020-06-17 14:17:51 +0530

    Added startup script for xrootd
    - Configured xrootd image built locally

---
+++ configmaps/xrootd/run/start.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -eux
+
+usage() {
+    cat << EOD
+
+Usage: `basename $0` [options] [cmd]
+
+  Available options:
+    -s <service> Service to start (cmsd/xrootd)
+    -h           Show Usage
+
+  Run cmsd/xrootd (ulimit setup).
+EOD
+}
+
+service=""
+
+# get the options
+while getopts hs: c ; do
+    case $c in
+        s) service="$OPTARG" ;;
+        \? | h) usage ; exit 2 ;;
+    esac
+done
+
+if [[ $# -eq 0 ]] || [[ -z "$service" ]] || egrep -qv "^(xrootd|cmsd)$" <<< "$service"; then
+    usage
+    exit 2
+fi
+
+shift $(($OPTIND - 1))
+
+CONFIG_DIR="/config-etc"
+XROOTD_CONFIG="$CONFIG_DIR/xrootd.cf"
+
+XROOTD_RDR_DN="{{.XrootdRedirectorDn}}"
+
+if hostname | egrep "^${XROOTD_RDR_DN}-[0-9]+$"; then
+    COMPONENT_NAME='manager'
+else
+    COMPONENT_NAME='worker'
+fi
+export COMPONENT_NAME
+
+# Start service
+#
+echo "Start $service"
+"$service" -c "$XROOTD_CONFIG" -n "$COMPONENT_NAME"
