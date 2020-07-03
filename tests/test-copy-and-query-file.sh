c64c8f43d913d052dedd8efbf96b6ffe18887e9f
Shivansh Saini <shivanshs9@gmail.com>
2020-07-03 20:00:44 +0530

    added xrdcp and xrdfs tests

--- tests/test-copy-and-query-file.sh
+++ tests/test-copy-and-query-file.sh
@@ -2,3 +2,9 @@
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 . $DIR/base.sh
+
+xrdfs root://base-xrootd-xrootd-redirector:1094/ ls /data
+
+xrdcp $DIR/dummy root://base-xrootd-xrootd-redirector:1094//data/dummy
+
+xrdfs root://base-xrootd-xrootd-redirector:1094/ ls /data
