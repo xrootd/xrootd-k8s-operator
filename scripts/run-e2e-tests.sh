c52c1123de4fa3895f42d745913bd138c5bbd281
Shivansh Saini <shivanshs9@gmail.com>
2020-07-09 01:02:44 +0530

    migrated image to qserv/xrootd

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -19,7 +19,7 @@ SHELL_POD="${INSTANCE}-client"
 
 # Run xrootd client pod
 kubectl delete pod -l "instance=$INSTANCE,tier=client" -n "$NAMESPACE"
-kubectl run "$SHELL_POD" --image="xrootd:latest" --image-pull-policy="IfNotPresent" --restart=Never sleep 3600 -n "$NAMESPACE"
+kubectl run "$SHELL_POD" --image="qserv/xrootd:latest" --image-pull-policy="IfNotPresent" --restart=Never sleep 3600 -n "$NAMESPACE"
 kubectl label pod "$SHELL_POD" "instance=$INSTANCE" "tier=client" -n "$NAMESPACE"
 
 while ! kubectl wait --for=condition=Ready pods -l "instance=$INSTANCE" -n "$NAMESPACE"
