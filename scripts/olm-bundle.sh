9d36cd9cf1fc2ad53edaf6d819b17855ec983b8f
Shivansh Saini <shivanshs9@gmail.com>
2020-08-05 23:16:09 +0530

    fix: get previous latest version when generating CSV bundle

--- scripts/olm-bundle.sh
+++ scripts/olm-bundle.sh
@@ -10,7 +10,7 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 # Create required bundle build and manifest directory
 mkdir -p $XROOTD_OPERATOR_BUNDLE_BUILD_DIR/manifests
 cp -r $XROOTD_OPERATOR_BUNDLE_DIR/* $XROOTD_OPERATOR_BUNDLE_BUILD_DIR
-cp -r $XROOTD_OPERATOR_BUNDLE_MANIFEST_DIR/* $XROOTD_OPERATOR_BUNDLE_BUILD_DIR/manifests/
+cp -r $XROOTD_OPERATOR_BUNDLE_MANIFEST_VERSION_DIR/* $XROOTD_OPERATOR_BUNDLE_BUILD_DIR/manifests/
 
 # Build the bundle container image
 echo "Building image ${XROOTD_OPERATOR_BUNDLE_IMAGE}"
