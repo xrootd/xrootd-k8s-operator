cc2cd1833cde4b3a311b3ffc926de8036a889696
Shivansh Saini <shivanshs9@gmail.com>
2020-08-23 20:58:44 +0530

    rename: controllers package to xrootdcontroller

--- controllers/xrootd/validations.go
+++ controllers/xrootd/validations.go
@@ -20,20 +20,3 @@ USA
 */
 
 package xrootdcontroller
-
-import (
-	"github.com/xrootd/xrootd-k8s-operator/pkg/utils/constant"
-	"github.com/xrootd/xrootd-k8s-operator/pkg/watch/xrootd"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-)
-
-func (r *XrootdClusterReconciler) RefreshWatch(request reconcile.Request) error {
-	reqLogger := r.Log.WithValues("xrootdcluster", request.NamespacedName)
-	reqLogger.Info("Watching Xrootd resources...")
-	return r.WatchManager.RefreshWatch(request)
-}
-
-func (r *XrootdClusterReconciler) AddXrootdLogger() {
-	r.AddWatchers(xrootd.NewLogsWatcher(constant.XrootdRedirector, r))
-	r.AddWatchers(xrootd.NewLogsWatcher(constant.XrootdWorker, r))
-}
